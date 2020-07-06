53daf4ec04fdf95481da741b8861a4399c0f396f
Mcneil <tm185221@ncr.com>
2020-07-06 18:13:39 -0400

    Opps

--- code/burger/views.py
+++ code/burger/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import http.client
+import requests
+from requests.auth import HTTPBasicAuth
+from  django.http import HttpResponse
+import config
+
+def index (request):
+
+    url = "https://gateway-staging.ncrcloud.com/provisioning/user-profiles"
+
+
+    headers={
+
+            'accept': 'application/json',
+            'nep-application-key': config.NEP_APPLICATION_KEY,
+        }
+    r = requests.get(url,auth=(config.USER, config.PASSWORD), headers = headers)
+
+    print(r.text)
+
+    return HttpResponse(r)
+
